26c0b7c2e71c0efe87e845447ebc60dc0504930d
Karen Langner <krenabeth@yahoo.com>
2016-07-15 09:36:18 -0400

    submit button

--- script.js
+++ script.js
@@ -14,6 +14,35 @@ $(document).ready(function(){
 
   }); // end onclick function
 
+//create array, prepopulate with 3 objects
+// var $seatArray = [];
+
+// $seatArray[12] = {
+//   name:
+// };
+// $ function seatAssignment(name, seat) {
+//   // this will connect with #name and #seat1...#seat24 from form
+//     var seat12 = {
+//       this.name: name, 
+//       this.seat: seat
+//   };
+
+// seatAssignment(Benson, seat13);
+//   {
+//     name: "Benson",
+//     seat: "seat13"
+//   };
+//   {
+//     name: "Karen",
+//     seat: "seat14"
+//   }
+
+
+
+// put in a few taken seats with user info
+
+//
+
 // when hovering over a red seat, display a message saying the seat is taken
 // needs to be tested after submit is turning class to red
 $( ".taken" ).hover(function() {
@@ -27,12 +56,14 @@ $(function() {
 	$('form').hide();
 });
 
+
 // set some of the seats to default as 'taken' class using jQuery
 // maybe need to use object
 var $submit = $('#submit');
 $($submit).on("click", function(){
-// make object
-
+// store user info in arry
+    var $name = document.getElementById('#name').val();
+    console.log($name);
 // change class of seat to red (div class 'seats' and id)
 	// var seatAssignment = $('.seats');
 	// need to set variable to id of seat selected and then use that variable below instead of 'this'
